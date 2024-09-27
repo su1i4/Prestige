104f4fdf6d1cf6a21d64a320d2b8444d463ef87e
sulemanlord <sulaimanmind862@gmail.com>
2024-09-27 12:32:25 +0600

    save

--- src/shared/ui/header/index.jsx
+++ src/shared/ui/header/index.jsx
@@ -1,9 +1,13 @@
 import './header.css'
+import { ReactComponent as Logo } from '../../icons/Logo.svg';
 
 export const Header = () => {
     return (
         <div className='Header'>
-            
+            <Logo />
+            <div className='routes' >
+
+            </div>
         </div>
     )
 }
