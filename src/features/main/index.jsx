6a3b87746de4146aebb8615aaebe98ee4e88d0c8
sulemanlord <sulaimanmind862@gmail.com>
2024-10-02 16:05:06 +0600

    save

--- src/features/main/index.jsx
+++ src/features/main/index.jsx
@@ -1,4 +1,21 @@
 import "./main.css";
+import { Geo } from "../../shared/icons/geo";
+import { Arrow } from "../../shared/icons/Arrow";
+import { Apartment } from "../../shared/icons/apartment";
+import { Dumbbell } from "../../shared/icons/dumbbell";
+import { Coffee } from "../../shared/icons/Coffe";
+import { File } from "../../shared/icons/File";
+import { Mic } from "../../shared/icons/Mic";
+import { Mark } from "../../shared/icons/Mark";
+import { Restaraunt } from "../../shared/icons/Restaraunt";
+import { Vector } from "../../shared/icons/Vector";
+import { Ruler } from "../../shared/icons/ruler";
+import { Next } from "../../shared/icons/Next";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 export const Main = () => {
   return (
@@ -15,12 +32,96 @@ export const Main = () => {
       </div>
       <div className="about">
         <div className="inner">
-            <h2>Об объекте</h2>
-            <div className="list">
-
+          <h2>Об объекте</h2>
+          <div className="list">
+            <div className="icons">
+              <div className="wrapp">
+                <Geo />
+                Територия: 21 сотка
+              </div>
+              <div className="wrapp">
+                <Ruler />
+                Плошадь: от 30.07 м2 до 625.27 м2
+              </div>
+              <div className="wrapp">
+                <Arrow />
+                Высота потолков: от 3 до 6м.
+              </div>
+              <div className="wrapp">
+                <Apartment />
+                Этажность: 13 этажей
+              </div>
+              <div style={{ marginTop: "50px" }} className="wrapp">
+                <File />
+                Красная книга
+              </div>
+              <div className="wrapp">
+                <Next />
+                Начало строительства : 3 кв. 2023 г
+              </div>
+              <div className="wrapp">
+                <Mark />
+                завершение строительства : 4 кв. 2026 г
+              </div>
+              <div style={{ marginTop: "50px" }} className="wrapp">
+                <Vector />
+                офисы под ключ
+              </div>
+              <div className="wrapp">
+                <Coffee />
+                Кофейня на 1 этаже
+              </div>
+              <div className="wrapp">
+                <Mic />
+                Конференц зал
+              </div>
+              <div className="wrapp">
+                <Dumbbell />
+                Спортивный зал
+              </div>
+              <div className="wrapp">
+                <Restaraunt />
+                Ресторан на 13 этаже
+              </div>
             </div>
+          </div>
         </div>
       </div>
+      <Swiper
+        // spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+      >
+        <SwiperSlide>
+          <div className="slide-content">
+            <h2>Холл</h2>
+            <p>
+              Репутация и значимость любого Бизнес-Центра начинается со входа,
+              ведь именно он создает первое впечатление
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide-content">
+            <h2>Slide 2</h2>
+            <p>Content for slide 2</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide-content">
+            <h2>Slide 3</h2>
+            <p>Content for slide 3</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide-content">
+            <h2>Slide 4</h2>
+            <p>Content for slide 4</p>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
---
+++ src/shared/icons/ruler.jsx
@@ -0,0 +1,19 @@
+export const Ruler = () => {
+  return (
+    <svg
+      width="36"
+      height="36"
+      viewBox="0 0 36 36"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13.5 22.5L4.935 31.065M4.5 10.5H7.5H4.5ZM4.5 18H7.5H4.5ZM31.5 22.5V31.5H6C5.60218 31.5 5.22064 31.342 4.93934 31.0607C4.65804 30.7794 4.5 30.3978 4.5 30V4.5H13.5V22.5H31.5ZM25.5 22.5V25.5V22.5ZM18 22.5V25.5V22.5Z"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
